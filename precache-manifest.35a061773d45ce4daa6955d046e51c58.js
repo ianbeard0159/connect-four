2f6e4a30ec820a2f55a38c75da681c056a53d428
Ian Beard <ianbeard0159@gmail.com>
2021-10-06 13:14:07 -0700

    Updates

---
+++ precache-manifest.35a061773d45ce4daa6955d046e51c58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ccf79e182f4a77da1a0b35a58140a3",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "3a3f22f5e83c42f08cf0",
+    "url": "/connect-four/static/css/main.e3015a4d.chunk.css"
+  },
+  {
+    "revision": "455af616f0b92ad65fd2",
+    "url": "/connect-four/static/js/2.ba3b88d8.chunk.js"
+  },
+  {
+    "revision": "3a3f22f5e83c42f08cf0",
+    "url": "/connect-four/static/js/main.3956d42d.chunk.js"
+  },
+  {
+    "revision": "104c3d16e41f41c94247",
+    "url": "/connect-four/static/js/runtime~main.988ae474.js"
+  }
+]);
